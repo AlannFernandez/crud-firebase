ef972037c11bcf92b15bf5b5067640d289ba8540
Alan <alanfernande8@gmail.com>
2022-02-09 02:45:46 -0300

    add some sets

--- index.js
+++ index.js
@@ -31,10 +31,10 @@ window.addEventListener("DOMContentLoaded", async (e) => {
     <p>${task.description}</p>
     <div>
       <button class="btn btn-primary btn-delete" data-id="${doc.id}">
-        🗑 Delete
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M17 6h5v2h-2v13a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V8H2V6h5V3a1 1 0 0 1 1-1h8a1 1 0 0 1 1 1v3zm1 2H6v12h12V8zm-9 3h2v6H9v-6zm4 0h2v6h-2v-6zM9 4v2h6V4H9z" fill="rgba(255,255,255,1)"/></svg>Delete
       </button>
       <button class="btn btn-secondary btn-edit" data-id="${doc.id}">
-        🖉 Edit
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M15.728 9.686l-1.414-1.414L5 17.586V19h1.414l9.314-9.314zm1.414-1.414l1.414-1.414-1.414-1.414-1.414 1.414 1.414 1.414zM7.242 21H3v-4.243L16.435 3.322a1 1 0 0 1 1.414 0l2.829 2.829a1 1 0 0 1 0 1.414L7.243 21z" fill="rgba(0,0,0,1)"/></svg> Edit
       </button>
     </div>
   </div>`;
